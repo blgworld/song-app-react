904b29426899d591e2d2dfc0d8b6ab25de5463a1
Bilguun <worldbilguun@gmail.com>
2020-05-19 20:33:43 +0800

    Updates

---
+++ precache-manifest.2708c98f08d147944d5fa4e2850767b4.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cbd97596412ab0e26ae5a31a490e3bb0",
+    "url": "/song-app-react/index.html"
+  },
+  {
+    "revision": "374bc53ba038c65471b4",
+    "url": "/song-app-react/static/css/main.c2725800.chunk.css"
+  },
+  {
+    "revision": "f0d34b331f0b1521c962",
+    "url": "/song-app-react/static/js/2.ea83e24d.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/song-app-react/static/js/2.ea83e24d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "374bc53ba038c65471b4",
+    "url": "/song-app-react/static/js/main.ec05b248.chunk.js"
+  },
+  {
+    "revision": "4114d701778f5e4b7915",
+    "url": "/song-app-react/static/js/runtime-main.9c614522.js"
+  },
+  {
+    "revision": "a6bb46ebab2bb2f65d4ace5c2b6fb910",
+    "url": "/song-app-react/static/media/1.a6bb46eb.gif"
+  },
+  {
+    "revision": "72606448b0161e5bfc70afec6c1b24b3",
+    "url": "/song-app-react/static/media/2.72606448.gif"
+  },
+  {
+    "revision": "b3746f85521fad258f3175160777054f",
+    "url": "/song-app-react/static/media/3.b3746f85.gif"
+  }
+]);
